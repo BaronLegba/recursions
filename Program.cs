ded22aa95e46a9b8d0be7f6f2e5ab202281c2634
BaronLegba <142911344+BaronLegba@users.noreply.github.com>
2023-11-27 21:16:26 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,3 +14,69 @@
             PrintNaturalNumbers(number - 1);
         }
     }
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+        
+        Console.Write("Введите значение M: ");
+        int M = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        if (M <= N)
+        {
+            int sum = CalculateSum(M, N);
+            Console.WriteLine($"Сумма натуральных чисел от {M} до {N}: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: M должно быть меньше или равно N.");
+        }
+
+        int CalculateSum(int start, int end)
+        {
+        if (start > end)
+        {
+            return 0;
+        }
+            else
+            {
+            return start + CalculateSum(start + 1, end);
+            }
+        }
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+        Console.Write("Введите значение m: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите значение n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = AckermannFunction(m, n);
+
+        Console.WriteLine($"Результат функции Аккермана для m={m} и n={n}: {result}");
+
+        int AckermannFunction(int m, int n)
+            {
+            if (m == 0)
+            {
+            return n + 1;
+            }
+            else if (m > 0 && n == 0)
+            {
+            return AckermannFunction(m - 1, 1);
+            }
+            else if (m > 0 && n > 0)
+            {
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+            }
+            else
+            {
+            return 0; 
+            }
+        }
